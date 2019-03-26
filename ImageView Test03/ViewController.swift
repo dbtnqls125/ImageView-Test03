5eaa66bc75f7d63b44ec0f8ad0c7b07ff9ba0d5b
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-03-26 15:34:33 +0900

    2019.3.26 -  Alien ImageView Update

--- ImageView Test03/ViewController.swift
+++ ImageView Test03/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MyImageView: UIImageView!
+    @IBOutlet weak var Mylabel: UILabel!
+    
+    var count = 1
+    var direction = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MyImageView.image = UIImage(named: "frame1/png")
+       
     }
 
-
+    @IBAction func changeImage(_ sender: Any) {
+        if count == 5 {
+            direction = false
+            
+        } else if count == 0 {
+            direction = true
+        }
+        
+        if direction == true {
+            count += 1
+        }else {
+            count -= 1
+        }
+        
+   //     if count == 5 {
+    //        count = 0
+   //          }
+    //count = count + 1
+        MyImageView.image = UIImage(named: "frame\(count).png")
+        
+        Mylabel.text = String(count)
+    }
+    
 }
 
